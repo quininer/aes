56016b47c4c2a8404c24bfdd0190bef6d7e08195
quininer kel <quininer@live.com>
2016-05-03 17:42:30 +0800

    fix(typo)

--- benches/encrypt.rs
+++ benches/encrypt.rs
@@ -14,7 +14,7 @@ use test::Bencher;
 
 
 #[bench]
-fn bench_openssl_ebc_encrypt(b: &mut Bencher) {
+fn bench_openssl_ecb_encrypt(b: &mut Bencher) {
     use openssl::crypto::symm::{ Crypter, Type, Mode };
 
     let key = rand!(16);
@@ -29,7 +29,7 @@ fn bench_openssl_ebc_encrypt(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_crypto_encrypt(b: &mut Bencher) {
+fn bench_crypto_ecb_encrypt(b: &mut Bencher) {
     use crypto::{ buffer, aes, blockmodes };
 
     let key = rand!(16);
@@ -56,5 +56,7 @@ fn bench_aes_encrypt(b: &mut Bencher) {
     let key = rand!(16);
     let plaintext = rand!(16);
 
-    b.iter(|| AES::new(&key).encrypt(&plaintext))
+    b.iter(|| {
+        AES::new(&key).encrypt(&plaintext);
+    });
 }
